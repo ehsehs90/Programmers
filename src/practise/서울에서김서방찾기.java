cd04db9706a280667041f2bfdddbfaddb3e201cf
dohyun <ehgus1551@naver.com>
2019-10-25 16:14:47 +0900

    equals

--- src/practise/서울에서김서방찾기.java
+++ src/practise/서울에서김서방찾기.java
@@ -2,11 +2,24 @@ package practise;
 
 public class 서울에서김서방찾기 {
 
-	static String[] seoul = new String[1000];
-	 static public String solution(String[] seoul) {
-	      String answer = "";
-	      return answer;
-	  }
+	// static String[] seoul = new String[1000];
+	static String[] seoul = { "", "Kim" };
+
+	static String answer = "";
+
+	static public String solution(String[] seoul) {
+		String k = "";
+
+		for (int i = 0; i < seoul.length; i++) {
+
+			if ("Kim".equals(seoul[i])) {
+				k = Integer.toString(i);
+			}
+		}
+		answer = "김서방은 " + k + "에 있다";
+		System.out.println(answer);
+		return answer;
+	}
 
 	public static void main(String[] args) {
 		solution(seoul);

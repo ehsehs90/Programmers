f5375008ff2a870e833d96217d5823dc7f431774
dohyun <ehgus1551@naver.com>
2019-10-25 15:51:56 +0900

    Integer.parseInt | Integer.toString

--- src/practise/문자열다루기기본.java
+++ src/practise/문자열다루기기본.java
@@ -2,29 +2,32 @@ package practise;
 
 public class 문자열다루기기본 {
 
-	static String s = "a234";
+	static String s = "1234";
 
 	static public boolean solution(String s) {
 		boolean answer = true;
-		
+		// System.out.println(answer);
 		// System.out.println(s.length());
-		if (s.length() == 4 | s.length() == 6) {
+		if (s.length() == 4 || s.length() == 6) {
 			for (int i = 0; i < s.length(); i++) {
 				char c = s.charAt(i);
 				// ASCII코드
+				System.out.println("C" + c);
 				if (c < '0' || c > '9') {
-					return false;
+					// System.out.println(answer);
+					answer = false;
+					System.out.println(answer);
+					return answer;
 				}
+				System.out.println(answer);
+				//return true;
 			}
-
 		}
+		else {
 			answer = false;
-		
-		
-		System.out.println(answer);
-		
+		}
+		// System.out.println(answer);
 		return answer;
-		
 	}
 
 	public static void main(String[] args) {
---
+++ src/문자열을정수로바꾸기.java
@@ -0,0 +1,23 @@
+
+public class 문자열을정수로바꾸기 {
+
+	static String s = "-234";
+
+	static public int solution(String s) {
+		int answer = 0;
+		String str = "";
+			answer =Integer.parseInt(s);
+			
+
+			
+			
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		solution(s);
+		System.out.println(solution(s));
+	}
+
+}
